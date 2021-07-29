3db0df0540df193b134a6c28186478a97905b3af
root <root@mail.karta-sverige.se>
2021-07-29 20:04:05 +0200

    treci commit.

--- cron.py
+++ cron.py
@@ -1,10 +1,15 @@
 from api import Apartman, db
-from api import parse_ics_url, ics_to_array
+from api import parse_ics_url, ics_to_array, naj_cleaning
 import json
+from datetime import datetime
 
 # .. . Get all apartments containing URL import, and exec import .. .. 
 
 apq = Apartman.query.all()
+
+# Run cleaning algorithm in the end - for all owners!
+gazde = []
+
 for ap in apq:
 
   # Import urls are in ap.conf
@@ -17,8 +22,26 @@ for ap in apq:
        tab = ics_to_array(cal)
 
        ap.calendar = json.dumps(tab)
+  
+       if ap.gazda_username not in gazde:
+         gazde.append(ap.gazda_username) 
 
      except Exception as e: 
-       print('* Error: bad URL for ' + ap.name + ' (' + ap.conf + ' * ' + str(e) + ')')
+       raise e
+       #print('* Error: bad URL for ' + ap.name + ' (' + ap.conf + ' * ' + str(e) + ')')
 
      db.session.commit()
+
+
+
+# Run algorithm
+
+for gazda in gazde:
+   print(' .. . running algorithm for ' + gazda);
+   out = naj_cleaning(gazda)
+   for ap in apq:
+     if ap.id in out:
+       ap.cleaning = json.dumps(out[ap.id])
+       if out[ap.id]: print('      -> ' + ap.cleaning)
+      
+db.session.commit() 
